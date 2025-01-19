581dce250a41620ee931d9031d1a7f76707e2350
sivaranjani-id11 <160419361+sivaranjani-id11@users.noreply.github.com>
2025-01-19 18:23:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function checkWeather(city) {
         } else if (data.weather[0].main == "Drizzle") {
             WeatherIcon.src = "images/drizzle.png";
         } else if (data.weather[0].main == "Mist") {
-            WeatherIcon.src = "images/mist.png";
+            WeatherIcon.src = "images/Thunder.png";
         }
 
         // Show weather details and hide error message
